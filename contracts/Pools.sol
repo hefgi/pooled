707184d236237216e32678a9b4a6c8191df71bde
François-Julien Alcaraz <fj@mokriya.com>
2018-06-01 15:36:13 +0200

    delete old contract and add event pool create to Pools contract

--- contracts/Pools.sol
+++ contracts/Pools.sol
@@ -5,15 +5,19 @@ import 'contracts/Pool.sol';
 
 contract Pools is Ownable {
 
-  	mapping (address => address[]) public owners;
+  mapping (address => address[]) public owners;
+
+  event poolCreated(address indexed pool, address indexed owner);
 
 	function Pools() public Ownable() {
 
 	}
 
-	function deployPool(string _name, string _author) {
+	function deployPool(string _name, string _author) public returns (address) {
 		address newPool = new Pool(_name, _author);
 		owners[msg.sender].push(newPool);
+		emit poolCreated(newPool, msg.sender);
+		return newPool;
 	}
 
 	function getPools(address addr) public view returns(address[]) {
